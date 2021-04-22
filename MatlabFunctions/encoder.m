e0ad483c147ecf4c3352be9497c00ee88cb11bb5
Chris Stitt <58044054+cstitt1@users.noreply.github.com>
2021-04-22 16:28:29 -0400

    Update encoder.m

--- MatlabFunctions/encoder.m
+++ MatlabFunctions/encoder.m
@@ -1,33 +1,52 @@
-%% ENCODE
+%% AudioEncoder Function
+% for random set of digitHVs where digits 0-9 --> HVs[1] - HVs[10]
+%       and bins where each is a decimal number
+%       and random set of positionHVs where indexed by bin position in
+%       audio sample (1st = HVs[1], 2nd = HVs[2], etc)
+%
+% scales bins to -1.0000 to 1.0000 then for each gets levelHV using
+%       BinEncoder with the digitHVs
+%
+% use levelHVs and multiply with positionHVs to get sum of products to be
+%       returned as audioHV by function
 
-max = -1;
-for i = 1:length(average)
-    if (abs(average(i)) > max)
-        max = abs(average(i));
+function [audioHV] = AudioEncoder(bins, digitHVs, positionHVs)
+    
+    bin_max = -1;
+    for i = 1:length(bins)
+        if (abs(bins(i)) > bin_max)
+            bin_max = abs(bins(i));
+        end
     end
-end
-
-for i = 1:length(average)
-    scaled(i) = average(i)/max;
-    scaled(i) = floor(scaled(i) * 10000)/10000;
+    
+    for i = 1:length(bins)
+        scaled_bins(i) = bins(i)/bin_max;
+        scaled_bins(i) = floor(scaled_bins(i) * 10000)/10000;
+    end
+    
+    for i = 1:length(scaled_bins)
+        levelHVs(i,:) = BinEncoder(scaled_bins(i), digitHVs);
+    end
+    
+    audioHV = levelHVs(1,:) .* positionHVs(1,:);
+    for i = 2:length(levelHVs(1))
+        audioHV = audioHV + levelHVs(i,:) .* positionHVs(i,:);
+    end
     
 end
 
-sum = HVEncoder(scaled(1),BaseVectors);
+%% BinEncoder Function
+% for random set of HVs where digits 0-9 --> HVS[1] - HVS[10]
+% get digits of bin (x.xxxx) by times 10,000 then mod 10 and div 10
+% use digits to get HV for each digit then perm and multiply for bin HV
 
-for i = 2:length(scaled)
-    sum = sum + HVEncoder(scaled(i),BaseVectors);
-end
+function [binHV] = BinEncoder(bin, digitHVs)
 
-sum
-
-function [binHV] = HVEncoder(bin, digitHVs)
-
-    bind = bin * 10000;
+    bin_digi = bin * 10000;
     
     for i=1:5
-        digitR(i) = mod(bind,10);
-        bind = floor(bind/10);
+        digitR(i) = mod(bin_digi,10);
+        bin_digi = floor(bin_digi/10);
     end
     
     product = digitHVs(digitR(5)+1,:);
