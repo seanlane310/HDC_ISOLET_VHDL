8d667483fbba5893572665b61b2d7d42ead07c92
seanlane310 <58016223+seanlane310@users.noreply.github.com>
2021-04-23 13:58:34 -0400

    Add files via upload
    Adds correlation to "Level HVs" (neighbors have positive correlation)

---
+++ MatlabFunctions/HVsWithCorrelation.m
@@ -0,0 +1,20 @@
+%% Project Frequency Bins onto Hypervectors
+D = 100;
+M = 10;
+for v = 1:10
+    if (v == 1)
+        BaseVectors(v,:) = randi([0,1],1,100);
+    else
+        BaseVectors(v,:) = BaseVectors(v-1,:);
+        rand = randi(M,(D/M-v),1);
+        for i = 1:length(rand)
+            BaseVectors(v,rand(i)) = ~BaseVectors(v,rand(i));
+        end
+    end
+end
+
+for e = 1:100
+    IDVectors(e,:) = randi([0,1],1,1000);
+end
+
+BaseVectors
